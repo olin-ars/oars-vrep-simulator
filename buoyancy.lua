ae689a38c50e98e81c6ee0f512ebffdb8da96e0a
TShapinsky <tobiasshapinsky@gmail.com>
2016-11-03 15:44:59 -0400

    Added comments to buoyancy code

--- buoyancy.lua
+++ buoyancy.lua
@@ -1,4 +1,3 @@
---module("buoyancy")
 local buoyancy = {}
 
 function buoyancy.init(voxel_size)
@@ -8,10 +7,13 @@ function buoyancy.init(voxel_size)
 end
 
 function buoyancy.do_buoyancy(boat)
+   --get boat position and velocity
    p=simGetObjectPosition(boat,-1)
    linVel, angVel = simGetObjectVelocity(boat)
+   --clear occupancy tree of old voxels
    simRemoveVoxelsFromOctree(submerged_volume,0,nil)
    simInsertObjectIntoOctree(submerged_volume,boat,0)
+   --remove voxels above water level
    local voxels = simGetOctreeVoxels(submerged_volume)
    local to_remove = {}
    local dz = simGetSimulationTimeStep()/2*linVel[3]
@@ -28,31 +30,35 @@ function buoyancy.do_buoyancy(boat)
    voxels = simGetOctreeVoxels(submerged_volume)
    local verts, indeces
    if table.getn(voxels) > 9 then
+      --construct a convex hull using voxels below water level
       verts,indeces = simGetQHull(voxels)
    end
+   --remove old mech from scene
    if shape ~= nil then
       simRemoveObject(shape)
       shape = nil
    end
    if verts ~= nil then
+      --create mesh from hull and calculate it's volume and center of mass
       shape = simCreateMeshShape(1,20,verts,indeces)
       local code = simComputeMassAndInertia(shape,1000)
       if code == 1 then
-	 
+	 --calculate buoyant force and transform it to correct reference frame
 	 local om = simGetObjectMatrix(boat,-1)
 	 local mass, inertia, cm = simGetShapeMassAndInertia(shape,om)
 	 local im = simInvertMatrix(om)
 	 local force = {0,0,mass*9.81};
 	 local appliedForce = simMultiplyVector(om,force)
+         --apply forces to boat
+         simAddForce(boat,cm,appliedForce)
+         --setup graph and plot debug data 
 	 simHandleGraph(graph_handle,simGetSimulationTime()+simGetSimulationTimeStep())
-	 --simResetGraph(graph_handle)
 	 simSetGraphUserData(graph_handle,"cm_x",cm[1])
 	 simSetGraphUserData(graph_handle,"cm_y",cm[2])
 	 simSetGraphUserData(graph_handle,"cm_z",cm[3])
 	 simSetGraphUserData(graph_handle,"f_x",appliedForce[1]/9.81/mass)
 	 simSetGraphUserData(graph_handle,"f_y",appliedForce[2]/9.81/mass)
 	 simSetGraphUserData(graph_handle,"f_z",appliedForce[3]/9.81/mass)
-	 simAddForce(boat,cm,appliedForce)
       end
    end
 end
